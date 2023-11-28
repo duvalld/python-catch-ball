d8a50057a4240f55463fc0616f44e2a119cbb97a
Duvall Danganan <duvalldanganan@gmail.com>
2023-11-28 16:35:19 +0800

    Add Welcome Message

--- ball_catch.py
+++ ball_catch.py
@@ -36,14 +36,14 @@ class Game:
         self._font = pygame.font.SysFont('Arial', 30)
         # game sound Setup
         # background music
-        self._bg_music = pygame.mixer.Sound('python-ball-catch/nba_sound.mp3')
+        self._bg_music = pygame.mixer.Sound('python-catch-ball/sound/nba_sound.mp3')
         self._bg_music.set_volume(.1)
         self._bg_music.play(loops = -1)
         # sound when player catch a ball
-        self._score_effect = pygame.mixer.Sound("python-ball-catch/score_effect.mp3")
+        self._score_effect = pygame.mixer.Sound("python-catch-ball/sound/score_effect.mp3")
         self._score_effect.set_volume(.1)
         # game over sound
-        self._gameover_sound = pygame.mixer.Sound("python-ball-catch/gameoverbuzzer.wav")
+        self._gameover_sound = pygame.mixer.Sound("python-catch-ball/sound/gameoverbuzzer.wav")
         self._gameover_sound.set_volume(.1)
     # game collision method, handles missed balls and update score
     def collision(self):
@@ -55,17 +55,24 @@ class Game:
             if ball_i._rect.colliderect(self._floor._rect):
                 # increse missed count
                 self._missed += 1
-                # remove balls that
+                # remove balls that collide to the floor
                 self._balls.remove(ball_i)
+                # add new ball instance to balls list
                 self._balls.append(Ball((randint(50, 300), -50), self._ball_speed))
+                # if player missed 3 balls call gameover method
                 if self._missed >= 3:
                     self.game_over()
             # ball collide to player
             elif ball_i._rect.colliderect(self._player._rect):
+                # remove balls that collide to the player
                 self._balls.remove(ball_i)
+                # add new ball instance to balls list
                 self._balls.append(Ball((randint(50, 300), -50), self._ball_speed))
+                # increse ball movement speed by .1
                 self._ball_speed += .1
+                # increse score count per ball
                 self._score += 1
+                # play sound effect for every catched ball
                 self._score_effect.play()
     # gameover method, handles gameover state
     def game_over(self):
@@ -96,61 +103,115 @@ class Game:
         # exit game
         pygame.quit
         exit()
-
-    # method where we render game elements to screen and call other game methods
-    def run(self):
+    # Welcome Screen
+    def welcom_screen(self):
         screen.fill("lightblue")
         screen.blit(self._player._image, self._player._rect)
         screen.blit(self._floor._image, self._floor._rect)
+        self._welcome_text = self._font.render("Catch Ball!", True, (255, 165, 0))
+        screen.blit(self._welcome_text, ((screen_width / 2) - (self._welcome_text.get_width() / 2), 200))
+        font_welcome = pygame.font.SysFont('Arial', 15)
+        welcome_text_list = [
+            "Dash and catch your way to a high score",
+            "in this heart-pounding ball-catching game,",
+            "but watch out miss three, and it's game over!"
+        ]
+        welcome_text_pos_height = 200 + self._welcome_text.get_height() + 5
+        for t in welcome_text_list:
+            self._mechanics_text = font_welcome.render(t, True, (0, 0, 0))
+            screen.blit(self._mechanics_text, ((screen_width / 2) - (self._mechanics_text.get_width() / 2), welcome_text_pos_height))
+            welcome_text_pos_height += self._mechanics_text.get_height() + 5
+
+        pygame.display.update()
+        sleep(5)
+    
+    # method where we render game elements to screen and call other game methods
+    def run(self):
+        # set background to color lightblue
+        screen.fill("lightblue")
+        # render player model on game screen
+        screen.blit(self._player._image, self._player._rect)
+        # render floor model on game
+        screen.blit(self._floor._image, self._floor._rect)
         self._score_text = self._font.render(f"Score: {self._score}", True, "white")
+        # render score text on game screen
         screen.blit(self._score_text, (5, 5))
         self._missed_text = self._font.render(f"Missed: {self._missed}", True, "red")
+        # render missed ball on game screen
         screen.blit(self._missed_text, (screen_width - self._missed_text.get_width(), 5))
+        # call player movement method
         self._player.movement()
+        # render ball object from balls list
         for ball in self._balls:
             screen.blit(ball._image, ball._rect)
+        # call collission method
         self.collision()
 
-# 
+# initialize Player Class
 class Player:
     def __init__(self, speed, floor_height):
-        self._image = pygame.image.load('python-ball-catch/player.png').convert_alpha()
+        # set player image
+        self._image = pygame.image.load('python-catch-ball/image/player.png').convert_alpha()
+        # transfrom player image scale
         self._image = pygame.transform.scale(self._image, (TILE_SIZE, TILE_SIZE * 2))
+        # set rectangle object for player image that we can use for positioning and collision
         self._rect = self._image.get_rect(center = (screen.get_width() / 2,screen.get_height() - floor_height - (self._image.get_height() / 2)))
+        # player movement per pixel
         self._speed = speed
-        
+
+    # Player movement method
     def movement(self):
         keys = pygame.key.get_pressed()
+        # move player to left/righ when left/righ arrow key is pressed and limit movement when end of the screen has been reached
         if keys[pygame.K_RIGHT] and self._rect.right < screen.get_width():
             self._rect.x += self._speed
         elif keys[pygame.K_LEFT] and self._rect.left > 0 :
             self._rect.x -= self._speed
-        
+
+# Floor class intialization
 class Floor:
     def __init__(self):
-        self._image = pygame.image.load('python-ball-catch/floor.png').convert_alpha()
+        # set floor image
+        self._image = pygame.image.load('python-catch-ball/image/floor.png').convert_alpha()
+        # transform floor image scale
         self._image = pygame.transform.scale(self._image, (TILE_SIZE * 15, TILE_SIZE * 5))
+        # set rectangle object for floor positioning and collision
         self._rect = self._image.get_rect(bottomleft = (0,screen.get_height()))
 
+# Ball class initialization
 class Ball:
     def __init__(self, position, speed):
-        self._image = pygame.image.load('python-ball-catch/ball.png').convert_alpha()
+        # set ball image
+        self._image = pygame.image.load('python-catch-ball/image/ball.png').convert_alpha()
+        # transform ball image scale
         self._image = pygame.transform.scale(self._image, (TILE_SIZE, TILE_SIZE))
+        # set rectangle object for ball positioning and collision
         self._rect = self._image.get_rect(topleft = position)
+        # ball movement per pixel
         self._ball_speed = speed
     
+    # ball movement method
     def move(self):
+        # move ball on y axis based on ball speed
         self._rect.y += self._ball_speed
 
 game = Game()
 running = True
 
+# welcome message
+game.welcom_screen()
+
+# Game loop
 while running:
+    # close game when close button is pressed
     for event in pygame.event.get():
         if event.type == pygame.QUIT:
             running = False
-            
+    
+    # call game run method
     game.run()
+    # limit game Frame Rate to 60
     clock.tick(60)
+    # update game screen per loop
     pygame.display.update()
     
